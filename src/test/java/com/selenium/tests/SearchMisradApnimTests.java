27eaf3a39bbf4ff6949e1dafe123a6d52d03cbfc
Michael Podoprigora <52321995+MikePodoprigora@users.noreply.github.com>
2021-01-24 22:45:15 -0500

    added tests

--- src/test/java/com/selenium/tests/SearchMisradApnimTests.java
+++ src/test/java/com/selenium/tests/SearchMisradApnimTests.java
@@ -15,8 +15,8 @@ public class SearchMisradApnimTests {
 
     @BeforeMethod
     public void setUp(){
-//        wd = new ChromeDriver(); // to chose what driver you want to use
-        wd = new FirefoxDriver();
+        wd = new ChromeDriver(); // to chose what driver you want to use
+//        wd = new FirefoxDriver();
         wd.manage().window().maximize(); // open your page to full size resolution
         wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         wd.navigate().to("https://www.autotrader.ca/");
@@ -31,6 +31,19 @@ public class SearchMisradApnimTests {
         // get page title
         String pageTitle = wd.findElement(By.cssSelector("h1 #titleText")).getText();
         System.out.println(pageTitle);
+
+        // click on Pop-up window "Goi It"
+        wd.findElement(By.cssSelector("#btnGotIt")).click();
+
+        // click on Logo button
+        wd.findElement(By.cssSelector("#logo")).click();
+
+        //click on Make field
+        wd.findElement(By.cssSelector("select#rfMakes")).click();
+
+        //choose Car category
+        wd.findElement(By.cssSelector("#rfMakes option[value=\"Volkswagen\"]")).click();
+
 
     }
 
