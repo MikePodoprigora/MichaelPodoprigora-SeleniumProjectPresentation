131610a06840d6a91ee9819ec1f10cd85688fc8d
Michael Podoprigora <52321995+MikePodoprigora@users.noreply.github.com>
2021-03-09 22:50:31 -0500

    Some changes

--- src/test/java/com/selenium/tests/SearchAutoTraderCarTests.java
+++ src/test/java/com/selenium/tests/SearchAutoTraderCarTests.java
@@ -3,20 +3,18 @@ package com.selenium.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class SearchMisradApnimTests {
+public class SearchAutoTraderCarTests {
     WebDriver wd;
 
     @BeforeMethod
     public void setUp(){
         wd = new ChromeDriver(); // to chose what driver you want to use
-//        wd = new FirefoxDriver();
         wd.manage().window().maximize(); // open your page to full size resolution
         wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         wd.navigate().to("https://www.autotrader.ca/");
@@ -26,25 +24,30 @@ public class SearchMisradApnimTests {
     @Test
     public void searchFromNavMenuTest(){
         // click on reception points
-        wd.findElement(By.cssSelector("#advancedSearch")).click();
+        click(By.cssSelector("#advancedSearch"));
 
         // get page title
         String pageTitle = wd.findElement(By.cssSelector("h1 #titleText")).getText();
         System.out.println(pageTitle);
 
         // click on Pop-up window "Goi It"
-        wd.findElement(By.cssSelector("#btnGotIt")).click();
+        click(By.cssSelector("#btnGotIt"));
 
         // click on Logo button
-        wd.findElement(By.cssSelector("#logo")).click();
+        click(By.cssSelector("#logo"));
 
         //click on Make field
-        wd.findElement(By.cssSelector("select#rfMakes")).click();
+        click(By.cssSelector("select#rfMakes"));
 
         //choose Car category
-        wd.findElement(By.cssSelector("#rfMakes option[value=\"Volkswagen\"]")).click();
+        click(By.cssSelector("#rfMakes option[value=\"Volkswagen\"]"));
 
 
+    }
+
+
+    public void click(By locator) {
+        wd.findElement(locator).click();
     }
 
 
